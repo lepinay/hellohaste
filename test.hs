d841d52eb07e17655822bf45e52a93c5e3abd353
Lepinay Laurent <lepinay@gmail.com>
2015-01-12 18:06:23 +0100

    first

---
+++ test.hs
@@ -0,0 +1,63 @@
+module Main where
+import Haste
+import Haste.JSON
+import Haste.Prim
+import Control.Applicative
+import MyModule
+
+
+data Address = Address
+    {
+        street::Maybe String,
+        city::Maybe String,
+        country::Maybe String
+    }
+    deriving (Show)
+
+data Person = Person
+    {
+        name::String,
+        firstname::String,
+        age::Maybe Int,
+        home::Maybe Address
+    }
+    deriving (Show)
+
+
+--child::JSON -> String -> (a->b) ->
+--child field ctor =
+--    case j .:? (toJSStr field) of
+--                    Parser(Right (Just json )) ->
+--                        Just
+--                        <$>
+
+
+
+--do
+--    home <- child j "home"
+--    Address
+--    <$> home .:? (toJSStr "street")
+--    <*> home .:? (toJSStr "city")
+--    <*> home .:? (toJSStr "country")
+
+main =
+    let ex1 = "{'name':'bob','firstname':'plop','home':{'city':'paris'}}"
+        ex2 = "{'name':'bob','firstname':'plop'}"
+        (Right j) = decodeJSON $ toJSStr ex1
+        (Parser (Right p)) =
+            Person
+            <$> j .: (toJSStr "name")
+            <*> j .: (toJSStr "firstname")
+            <*> j .:? (toJSStr "age")
+            <*> do
+                    phome <- j .:? (toJSStr "home")
+                    do
+                        home <- phome
+                        Just
+                        <$> (Address
+                        <$> home .:? (toJSStr "street")
+                        <*> home .:? (toJSStr "city")
+                        <*> home .:? (toJSStr "country"))
+    in putStrLn $ show p
+
+
